57cdda945fca347007febd5616a17c8daac78879
AllisonWinter-Reed <38668004+AllisonWinter-Reed@users.noreply.github.com>
2020-02-22 15:45:19 -0600

    Finished through part 12

--- app/src/main/java/com/example/lab4milestone2/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone2/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.lab4milestone2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startListening();
+        }
+    }
+
+    public void updateLocationInfo(Location location) {
+
+        Log.i("LocationInfo", location.toString());
+
+        TextView latTextView = (TextView) findViewById(R.id.latitude);
+
+        TextView lonTextView = (TextView) findViewById(R.id.longitude);
+
+        TextView altTextView = (TextView) findViewById(R.id.altitude);
+
+        TextView accTextView = (TextView) findViewById(R.id.accuracy);
+
+        latTextView.setText("Latitude: " + location.getLatitude());
+
+        lonTextView.setText("Longitude: " + location.getLongitude());
+
+        altTextView.setText("Altitude: " + location.getAltitude());
+
+        accTextView.setText("Accuracy: " + location.getAccuracy());
+
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {}
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
+        startListening();
+
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            //added later
+        }
     }
+
 }
